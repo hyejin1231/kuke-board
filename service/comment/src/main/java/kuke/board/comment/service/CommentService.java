18a122f764b3036170a8b86cc7b111c7a01c6543
hyejin1231 <phj981231@naver.com>
2025-08-20 15:01:47 +0900

    게시글 댓글 최대 2-depth - CUD API 구현

---
+++ service/comment/src/main/java/kuke/board/comment/service/CommentService.java
@@ -0,0 +1,80 @@
+package kuke.board.comment.service;
+
+import kuke.board.comment.entity.Comment;
+import kuke.board.comment.repository.CommentRepository;
+import kuke.board.comment.service.request.CommentCreateRequest;
+import kuke.board.comment.service.response.CommentResponse;
+import kuke.board.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static java.util.function.Predicate.not;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+    private final Snowflake snowflake = new Snowflake();
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public CommentResponse create(CommentCreateRequest request) {
+        Comment parent = findParent(request);
+        Comment comment = commentRepository.save(
+                Comment.create(
+                        snowflake.nextId(),
+                        request.getContent(),
+                        parent == null ? null : parent.getParentCommentId(),
+                        request.getArticleId(),
+                        request.getWriterId()
+                )
+        );
+
+        return CommentResponse.from(comment);
+    }
+
+    private Comment findParent(CommentCreateRequest request) {
+        Long parentCommentId = request.getParentCommentId();
+        if (parentCommentId == null) {
+            return null;
+        }
+        return commentRepository.findById(parentCommentId)
+                .filter(not(Comment::getDeleted))
+                .filter(Comment::isRoot)
+                .orElseThrow();
+    }
+
+    public CommentResponse read(Long commentId) {
+        return CommentResponse.from(
+                commentRepository.findById(commentId).orElseThrow()
+        );
+    }
+
+    @Transactional
+    public void delete(Long commentId) {
+        commentRepository.findById(commentId)
+                .filter(not(Comment::getDeleted))
+                .ifPresent(comment -> {
+                    if (hasChildren(comment)) {
+                        comment.delete(); // 자식 댓글이 있으면 삭제 플래그만 (소프트 delete)
+                    }else {
+                        delete(comment); // 자식 댓글 없으면 DB 삭제
+                    }
+                });
+    }
+
+    private boolean hasChildren(Comment comment) {
+        return commentRepository.countBy(comment.getArticleId(), comment.getCommentId(), 2L) == 2 ;
+    }
+
+    private void delete(Comment comment) {
+        commentRepository.delete(comment); // 실제 삭제
+
+        if (!comment.isRoot()) {
+            commentRepository.findById(comment.getParentCommentId())
+                    .filter(Comment::getDeleted) // 부모가 이미 소프트 삭제 상태라면
+                    .filter(not(this::hasChildren)) // 부모가 더 이상 자식이 없다면
+                    .ifPresent(this::delete); // 재귀적으로 부모도 삭제
+        }
+    }
+}
