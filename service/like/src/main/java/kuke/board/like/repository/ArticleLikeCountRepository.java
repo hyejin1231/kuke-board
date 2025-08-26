6414edc275a8e2e85a6f0f0161dd1ab574938e7a
hyejin1231 <phj981231@naver.com>
2025-08-26 10:09:18 +0900

    좋아요 수 구현

---
+++ service/like/src/main/java/kuke/board/like/repository/ArticleLikeCountRepository.java
@@ -0,0 +1,34 @@
+package kuke.board.like.repository;
+
+import jakarta.persistence.LockModeType;
+import kuke.board.like.entity.ArticleLikeCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ArticleLikeCountRepository extends JpaRepository<ArticleLikeCount, Long> {
+
+    // select..for update 비관적 락
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<ArticleLikeCount> findLockedByArticleId(Long articleId);
+
+    @Query(
+            value = "update article_like_count set like_count = like_count + 1 where article_id = :articleId",
+            nativeQuery = true
+    )
+    @Modifying // update 쿼리에는 이 어노테이션이 필요하다고 함
+    int increase(@Param("articleId") Long articleId);
+
+    @Query(
+            value = "update article_like_count set like_count = like_count - 1 where article_id = :articleId",
+            nativeQuery = true
+    )
+    @Modifying // update 쿼리에는 이 어노테이션이 필요하다고 함
+    int decrease(@Param("articleId") Long articleId);
+}
